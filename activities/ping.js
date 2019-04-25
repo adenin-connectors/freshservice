a39f9056c841b7b5abceeed81ebe88780899cef4
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-25 10:01:46 +0200

    Fixed ping when api key is wrong.

--- activities/ping.js
+++ activities/ping.js
@@ -4,12 +4,12 @@ const api = require('./common/api');
 module.exports = async (activity) => {
   try {
     api.initialize(activity);
-    const response = await api('/itil/requesters.json');
+    const response = await api('/agents.json');
 
     if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data = {
-      success: response && response.statusCode === 200
+      success: response && response.statusCode === 200 && !response.body.require_login
     };
   } catch (error) {
     $.handleError(activity, error);
